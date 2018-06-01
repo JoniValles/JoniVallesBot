e30455f013d9a7c7482d51b7ef3d0cd62f3cd300
Joni <jonivalles@gmail.com>
2018-06-01 21:10:08 +0200

    ID

--- index.php
+++ index.php
@@ -402,7 +402,7 @@ while ($row = mysqli_fetch_array($result)) {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
     		'chat_id' => $update->message->chat->id,
-    		'text' => "Nº: " .$row['id']." - Recompensa: " . $row['Recompensa'] . " - Pokeparada: ". $row['Pokeparada']
+    		'text' => "N: " .$row['id']." - Recompensa: " . $row['Recompensa'] . " - Pokeparada: ". $row['Pokeparada']
     		]);
 
     }
