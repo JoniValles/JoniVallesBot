c99752a563e794e5162f0ae897fde3668d7b22a8
Joni <jonivalles@gmail.com>
2018-06-01 20:09:58 +0200

    Añadir acabado2

--- index.php
+++ index.php
@@ -488,7 +488,7 @@ $conn->set_charset("utf8");
 $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
     		'chat_id' => $update->message->chat->id,
-    		'text' => "Misión añadida!"
+    		'text' => "Misión confirmada!"
     		]);
 	}
 	
