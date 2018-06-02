c48382c3c3154006e5e2590929e5473a9f2d7b9e
Joni <jonivalles@gmail.com>
2018-06-02 13:20:58 +0200

    Prueba

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 *
 */
 require 'vendor/autoload.php';
-
+use Telegram\Bot\Api;
 $client = new Zelenin\Telegram\Bot\Api('544381336:AAGnNEVqil8XIxMUyd61wSOUZVM3thxzTNQ'); // Set your access token
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
