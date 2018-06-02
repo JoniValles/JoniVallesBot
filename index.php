5c03bc1285d2dd452c37cb7c66db76712b873a26
Joni <jonivalles@gmail.com>
2018-06-02 13:30:40 +0200

    Revert

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ require 'vendor/autoload.php';
 $client = new Zelenin\Telegram\Bot\Api('544381336:AAGnNEVqil8XIxMUyd61wSOUZVM3thxzTNQ'); // Set your access token
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
-$telegram = new Api('TELEGRAM_TOKEN');
  
 
 
