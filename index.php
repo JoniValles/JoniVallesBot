10b3839936aca710a347df9281417da0f0b322a0
Joni <jonivalles@gmail.com>
2018-06-02 13:17:49 +0200

    Prueba

--- index.php
+++ index.php
@@ -105,26 +105,13 @@ try {
     }else if($update->message->text == '/prueba') {
 $telegram = new Api('TELEGRAM_TOKEN');
 
-			$keyboard = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-         ['0']
-];
-
-$reply_markup = $telegram->replyKeyboardMarkup([
-    'keyboard' => $keyboard, 
-    'resize_keyboard' => true, 
-    'one_time_keyboard' => true
-]);
-
-$response = $telegram->sendMessage([
-    'chat_id' => 'CHAT_ID', 
-    'text' => 'Hello World', 
-    'reply_markup' => $reply_markup
+		$response = $telegram->sendMessage([
+    'chat_id' => $update->message->chat->id,
+    'text' => 'Hello World'
 ]);
 
 $messageId = $response->getMessageId();
+
 
 
     }
