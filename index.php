6f3d4c4f82ed08d078801a9ecdd0c72c0da1f486
Joni <jonivalles@gmail.com>
2018-06-03 12:25:51 +0200

    a

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
 *
 */
 require 'vendor/autoload.php';
-$client = new Zelenin\Telegram\Bot\Api('544381336:AAGnNEVqil8XIxMUyd61wSOUZVM3thxzTNQ'); // Set your access token
+$client = new Zelenin\Telegram\Bot\Api('544381336:AAH_gAsgfCY6EzSlULTRi0cJJIUhlmWsB9Y'); // Set your access token
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
-$bot = new \TelegramBot\Api\BotApi('544381336:AAGnNEVqil8XIxMUyd61wSOUZVM3thxzTNQ');
+//$bot = new \TelegramBot\Api\BotApi('544381336:AAH_gAsgfCY6EzSlULTRi0cJJIUhlmWsB9Y');//
 
  
 
