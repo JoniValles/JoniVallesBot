ec8ca04d078546cec91f22fb082ee7b2c429d6f4
Joni <jonivalles@gmail.com>
2018-06-03 23:36:01 +0200

    asda

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 *
 */
 require 'vendor/autoload.php';
-$client = new Zelenin\Telegram\Bot\Api('544381336:AAGA-5dftz6frX-P2j2r21vdgS7tU_YDOvE'); // Set your access token
+$client = new Zelenin\Telegram\Bot\Api('544381336:AAGq_Z9pdknzO_q9n6ZVlID-2SWMB4HS9I4'); // Set your access token
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
  
