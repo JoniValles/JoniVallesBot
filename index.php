7ab31b1991f4fa8fe94c2855dce88df1b24cb4b6
Joni <jonivalles@gmail.com>
2018-06-05 02:02:15 +0200

    dasd

--- index.php
+++ index.php
@@ -584,26 +584,20 @@ $data = "Todavia no se han añadido misiones";
  
 while ($row = mysqli_fetch_array($result)) 
 	$data = $row['Pokeparada'];
+	$data2 = $row2['Name'];
 	$response = $client->sendMessage([
     		'chat_id' => $update->message->chat->id,
 			'parse_mode' => 'HTML',
-    		'text' => $data
+    		'text' => $data . $data2
     		]);
 	while ($row2 = mysqli_fetch_array($result2)) {
-		//$data = "O aqui";
-		$data2 = mysqli_num_rows($result2);
-	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-			'parse_mode' => 'HTML',
-    		'text' => $data2
-    		]);
 		similar_text($row['Pokeparada'], $row2['Name'], $percent);
 		 if($percent > 0){
 			//$query3 = "insert into mision (Latitude, Longitude) values ('$row2[Longitude]','$row[Latitude]');";
 			//$query3 = "insert into mision (Latitude, Longitude) values ('$row2[Longitude]','$row[Latitude]');";
 			//$result4 = mysqli_query($conn, $query3);
 
-			//$data = "Bien";
+			$data = "Bien";
     }else{
 		//$data = "Mal";
 	}
