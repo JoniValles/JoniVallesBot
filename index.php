b67c4e31350194c6ae3de40428c8c56dfe093a3e
Joni <jonivalles@gmail.com>
2018-06-05 20:02:40 +0200

    Delete location

--- index.php
+++ index.php
@@ -975,6 +975,47 @@ $conn->set_charset("utf8");
 	}
 	
 	
+	else if(substr($update->message->text, 0, 13) === "/delubicacion")
+    {
+		
+		
+		 //connecting to database and getting the connection object
+//database constants
+ define('DB_HOST', 'den1.mysql2.gear.host');
+ define('DB_USER', 'pmgmisiones');
+ define('DB_PASS', "Mw78_Gz8-CJs");
+ define('DB_NAME', 'PMGMisiones');
+ 
+ //connecting to database and getting the connection object
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn->set_charset("utf8");
+ if (mysqli_connect_errno()) {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ //$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => ERROR
+    		]);
+ die();
+ http_response_code(200);
+ }
+ 
+ 
+ $id = explode(" ", $update->message->text);
+ $query = "update misionoviedo set Latitude = NULL, Longitude = NULL where id = $id[1] ";
+ //executing the query 
+ mysqli_query($conn, $query) or die('Error querying database.');
+//$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => "Mision borrada!"
+    		]);
+			 http_response_code(200);
+	}
+	
+	
 	
 	
 	
